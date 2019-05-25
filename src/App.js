65b45bba93cbfc77acae5b1ffc58b10bf39cff61
Max <mjann@ucsc.edu>
2019-05-25 09:05:05 -0700

    maja

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import red from './red.jpg';
+
 import './App.css';
 
 class App extends Component {
@@ -26,6 +28,20 @@ class App extends Component {
             Jann Software
           </a>
         </header>
+        <div>
+          <div>
+            <p>Part 2</p>
+            <img src={red}/>
+          </div>
+        </div>
+        <div>
+        <p>Part 3</p>
+          <div id="red">
+            <div>
+              <p id="seethrough">Transluscent Text</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
